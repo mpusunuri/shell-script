985256ad03fe301d9c98e33db2d085dedbc0d22f
mpusunuri <mpusunuri@gmail.com>
2025-03-07 11:16:49 +0530

    Kiran and Madhu script added

---
+++ variables4.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+PERSON1=$1
+PERSON2=$2
+
+echo  "$PERSON1:: Hi $PERSON2"
+echo  "$PERSON2:: Hi $PERSON1"
+echo  "$PERSON1:: How are you?"
+echo  "$PERSON2:: I am fine, How are you?"
+echo  "$PERSON1:: I am also fine"
+echo  "$PERSON2:: Bye"
+echo  "$PERSON1:: Bye"
+echo  "$PERSON2:: Take care"
+echo  "$PERSON1:: You too"
