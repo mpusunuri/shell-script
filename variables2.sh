f25a4d2e3d00933e0136286c359fb480b9ec863a
mpusunuri <mpusunuri@gmail.com>
2025-03-07 10:38:24 +0530

    Raju and Rakesh conversations added

---
+++ variables2.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo  "Raju:: Hi Rakesh"
+echo  "Rakesh:: Hi Raju"
+echo  "Raju:: How are you?"
+echo  "Rakesh:: I am fine, How are you?"
+echo  "Raju:: I am also fine"
+echo  "Rakesh:: Bye"
+echo  "Raju:: Bye"
+echo  "Rakesh:: Take care"
+echo  "Raju:: You too"
