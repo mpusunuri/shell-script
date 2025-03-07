371cd02e19adffd141484a8d7ef7ea85d98365ad
mpusunuri <mpusunuri@gmail.com>
2025-03-07 12:45:43 +0530

    Data Types script2 added

--- datatypes1.sh
+++ datatypes1.sh
@@ -5,19 +5,19 @@ NUMBER2=$2
 
 TIMESTAMP=$(date)
 echo "script executed at: $TIMESTAMP"
-SUM=$(($NUMBER + $NUMBER2))
+SUM=$(($NUMBER1 + $NUMBER2))
 
 echo "The sum of $NUMBER1 and $NUMBER2 is: $SUM"
-DIFFERENCE=$(($NUMBER - $NUMBER2))
+DIFFERENCE=$(($NUMBER1 - $NUMBER2))
 
 echo "The difference of $NUMBER1 and $NUMBER2 is: $DIFFERENCE"
-PRODUCT=$(($NUMBER * $NUMBER2))
+PRODUCT=$(($NUMBER1 * $NUMBER2))
 
 echo "The product of $NUMBER1 and $NUMBER2 is: $PRODUCT"
-QUOTIENT=$(($NUMBER / $NUMBER2))
+QUOTIENT=$(($NUMBER1 / $NUMBER2))
 
 echo "The quotient of $NUMBER1 and $NUMBER2 is: $QUOTIENT"
-REMAINDER=$(($NUMBER % $NUMBER2))
+REMAINDER=$(($NUMBER1 % $NUMBER2))
 
 echo "The remainder of $NUMBER1 and $NUMBER2 is: $REMAINDER"
 
